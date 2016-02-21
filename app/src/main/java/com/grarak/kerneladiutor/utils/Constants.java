58576664029728fb984b6f0575a2496f80dc809b
Willi Ye <williye97@gmail.com>
2016-02-21 18:36:26 +0100

    Clean vibration array

--- app/src/main/java/com/grarak/kerneladiutor/utils/Constants.java
+++ app/src/main/java/com/grarak/kerneladiutor/utils/Constants.java
@@ -663,32 +663,20 @@ public interface Constants {
     // Misc
 
     // Vibration
-    String[] VIBRATION_ARRAY = {
-            "/sys/vibrator/pwmvalue",
-            "/sys/class/timed_output/vibrator/amp",
-            "/sys/class/timed_output/vibrator/level",
-            "/sys/class/timed_output/vibrator/vtg_level",
-            "/sys/devices/platform/tspdrv/nforce_timed",
-            "/sys/class/timed_output/vibrator/pwm_value",
-            "/sys/devices/i2c-3/3-0033/vibrator/vib0/vib_duty_cycle",
-            "/sys/devices/virtual/timed_output/vibrator/voltage_level",
-            "/sys/devices/virtual/timed_output/vibrator/pwm_value_1p",
-            "/sys/devices/virtual/timed_output/vibrator/vmax_mv_strong",
-            "/sys/devices/virtual/timed_output/vibrator/vmax_mv"
-    };
-
-    int[][] VIBRATION_MAX_MIN_ARRAY = {
-            {127, 0},
-            {100, 0},
-            {31, 12},
-            {31, 12}, // Read MAX MIN from sys
-            {127, 1},
-            {100, 0}, // Read MAX MIN from sys
-            {100, 25}, // Needs enable path
-            {3199, 1200},
-            {99, 53},
-            {3596, 116}, // Needs VIB_LIGHT path
-            {3596, 116}
+    Object[][] VIBRATION_ARRAY = {
+            // {Path, Max, Min}
+            {"/sys/class/timed_output/vibrator/amp", 100, 0},
+            {"/sys/class/timed_output/vibrator/level", 31, 12},
+            {"/sys/class/timed_output/vibrator/pwm_value", 100, 0}, // Read MAX MIN from sys
+            {"/sys/class/timed_output/vibrator/pwm_value_1p", 99, 53},
+            {"/sys/class/timed_output/vibrator/voltage_level", 3199, 1200},
+            {"/sys/class/timed_output/vibrator/vtg_level", 31, 12}, // Read MAX MIN from sys
+            {"/sys/class/timed_output/vibrator/vmax_mv", 3596, 116},
+            {"/sys/class/timed_output/vibrator/vmax_mv_strong", 3596, 116}, // Needs VIB_LIGHT path
+            {"/sys/devices/platform/tspdrv/nforce_timed", 127, 1},
+            {"/sys/devices/i2c-3/3-0033/vibrator/vib0/vib_duty_cycle", 100, 25}, // Needs enable path
+            {"/sys/module/qpnp_vibrator/parameters/vib_voltage", 31, 12},
+            {"/sys/vibrator/pwmvalue", 127, 0}
     };
 
     String VIB_ENABLE = "/sys/devices/i2c-3/3-0033/vibrator/vib0/vib_enable";
@@ -754,10 +742,12 @@ public interface Constants {
     String TCP_AVAILABLE_CONGESTIONS = "/proc/sys/net/ipv4/tcp_available_congestion_control";
     String HOSTNAME_KEY = "net.hostname";
 
-    String[][] MISC_ARRAY = {{VIB_ENABLE, SENSOR_IND_WAKELOCK, MSM_HSIC_HOST_WAKELOCK, WLAN_RX_WAKELOCK_DIVIDER,
+    Object[][] MISC_ARRAY = {
+            VIBRATION_ARRAY,
+            {VIB_LIGHT, VIB_ENABLE, SENSOR_IND_WAKELOCK, MSM_HSIC_HOST_WAKELOCK, WLAN_RX_WAKELOCK_DIVIDER,
             MSM_HSIC_WAKELOCK_DIVIDER, LOGGER_ENABLED, DYNAMIC_FSYNC, GENTLE_FAIR_SLEEPERS, POWER_SUSPEND_MODE,
             POWER_SUSPEND_STATE, TCP_AVAILABLE_CONGESTIONS, HOSTNAME_KEY},
-            SMB135X_WAKELOCKS, WLAN_RX_WAKELOCKS, WLAN_CTRL_WAKELOCKS, WLAN_WAKELOCKS, VIBRATION_ARRAY, CRC_ARRAY, FSYNC_ARRAY};
+            SMB135X_WAKELOCKS, WLAN_RX_WAKELOCKS, WLAN_CTRL_WAKELOCKS, WLAN_WAKELOCKS, CRC_ARRAY, FSYNC_ARRAY};
 
     // Build prop
     String BUILD_PROP = "/system/build.prop";
