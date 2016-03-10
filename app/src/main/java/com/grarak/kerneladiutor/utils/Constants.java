f55c51042fb09eff3a1bf7519ae640724eb70a5a
benschhold <benschhold@gmail.com>
2016-03-10 19:50:11 +0100

    Add switch for Boeffla Config Helper
    Signed-off-by: Martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	app/src/main/java/com/grarak/kerneladiutor/fragments/kernel/CPUHotplugFragment.java
    	app/src/main/java/com/grarak/kerneladiutor/utils/Constants.java

--- app/src/main/java/com/grarak/kerneladiutor/utils/Constants.java
+++ app/src/main/java/com/grarak/kerneladiutor/utils/Constants.java
@@ -95,7 +95,7 @@ public interface Constants {
     String MAXLITTLE = "/sys/devices/system/cpu/cpu0/core_ctl/max_cpus";
 
    String[] CORECONTROL_ARRAY = {MINBIG, MINLITTLE, MAXBIG, MAXLITTLE, BCH};
-    // CPU Voltage
+  // CPU Voltage
     String CPU_VOLTAGE = "/sys/devices/system/cpu/cpu0/cpufreq/UV_mV_table";
     String CPU_VDD_VOLTAGE = "/sys/devices/system/cpu/cpu0/cpufreq/vdd_levels";
     String CPU_FAUX_VOLTAGE = "/sys/devices/system/cpu/cpufreq/vdd_table/vdd_levels";
@@ -105,6 +105,7 @@ public interface Constants {
     String[] CPU_VOLTAGE_ARRAY = {CPU_VOLTAGE, CPU_VDD_VOLTAGE, CPU_FAUX_VOLTAGE, CPU_OVERRIDE_VMIN};
 
     // CPU Hotplug
+    String BCH = "/sys/kernel/boeffla_config_mode/enabled";
     String HOTPLUG_MPDEC = "mpdecision";
 
     String HOTPLUG_INTELLI_PLUG = "/sys/module/intelli_plug/parameters";
