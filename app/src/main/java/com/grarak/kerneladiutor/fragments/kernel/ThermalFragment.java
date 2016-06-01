d249e5ae0a31a25797d9ee787736744986379f8a
Yoinx <joseph.schubert@gmail.com>
2016-06-01 09:17:03 -0400

    THermal: Remove derp

--- app/src/main/java/com/grarak/kerneladiutor/fragments/kernel/ThermalFragment.java
+++ app/src/main/java/com/grarak/kerneladiutor/fragments/kernel/ThermalFragment.java
@@ -628,7 +628,6 @@ public class ThermalFragment extends RecyclerViewFragment implements SwitchCardV
             Thermal.setFrancoThermalStageThree(CPU.getFreqs().get(position), getActivity());
         else if (dPopupCard == mFrancoThermalStageFourCard)
             Thermal.setFrancoThermalStageFour(CPU.getFreqs().get(position), getActivity());
-        getActivity().getSupportFragmentManager().beginTransaction().notifyAll();
     }
 
     @Override
